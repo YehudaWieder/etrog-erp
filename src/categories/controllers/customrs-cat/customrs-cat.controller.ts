9bb33c381fe9073161e769086576c57b86e77421
YehudaWieder <v4105677@gmail.com>
2026-04-19 13:59:44 +0300

    Add customer-cat controller

--- src/categories/controllers/customrs-cat/customrs-cat.controller.ts
+++ src/categories/controllers/customrs-cat/customrs-cat.controller.ts
@@ -1,4 +1,58 @@
-import { Controller } from '@nestjs/common';
+// src/categories/controllers/customer-cat/customer-cat.controller.ts
 
-@Controller('customrs-cat')
-export class CustomrsCatController {}
+import { Controller, Get, Post, Patch, Delete, Body, Param, ParseIntPipe, Query } from '@nestjs/common';
+import { CustomerCatService } from 'src/categories/services/customrs-cat/customrs-cat.service';
+import { Prisma } from '@prisma/client';
+
+@Controller('customer-categories')
+export class CustomerCatController {
+  constructor(private readonly customerCatService: CustomerCatService) {}
+
+  @Post()
+  setCustomerCategoryAndPrice(@Body() data: { 
+    seasonId: number; 
+    customerId: number; 
+    name: string; 
+    grade: any; 
+    price: number; 
+    currency: any 
+  }) {
+    return this.customerCatService.setPrice(data);
+  }
+
+  @Get('customer/:customerId')
+  findByCustomer(
+    @Param('customerId', ParseIntPipe) customerId: number,
+    @Query('seasonId', ParseIntPipe) seasonId: number,
+  ) {
+    return this.customerCatService.findByCustomer(customerId, seasonId);
+  }
+
+  @Get('by-customer-and-name-grade')
+  findByCustomerAndNameGrade(
+    @Query('customerId', ParseIntPipe) customerId: number,
+    @Query('seasonId', ParseIntPipe) seasonId: number,
+    @Query('name') name: string,
+    @Query('grade') grade: any,
+  ) {
+    return this.customerCatService.findByCustomerAndNameGrade(customerId, seasonId, name, grade);
+  }
+
+  @Get('season/:seasonId')
+  findAllBySeason(@Param('seasonId', ParseIntPipe) seasonId: number) {
+    return this.customerCatService.findAllBySeason(seasonId);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateData: Prisma.CustomerCategoriesUpdateInput,
+  ) {
+    return this.customerCatService.update(id, updateData);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.customerCatService.remove(id);
+  }
+}
