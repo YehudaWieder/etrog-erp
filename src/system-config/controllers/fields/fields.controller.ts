9b89f2f186e6400ec52f34ce8777368f061fc6cd
YehudaWieder <v4105677@gmail.com>
2026-04-19 11:59:21 +0300

    Add field controller

--- src/system-config/controllers/fields/fields.controller.ts
+++ src/system-config/controllers/fields/fields.controller.ts
@@ -1,4 +1,39 @@
-import { Controller } from '@nestjs/common';
+// src/system-config/controllers/fields/fields.controller.ts
+
+import {
+  Controller, Get, Post, Delete, Patch, Body, Param,} from '@nestjs/common';
+import { FieldService } from 'src/system-config/services/fields/fields.service';
 
 @Controller('fields')
-export class FieldsController {}
+export class FieldController {
+  constructor(private readonly fieldService: FieldService) {}
+
+  // Get all fields
+  @Get()
+  getAllFields() {
+    return this.fieldService.getAllFields();
+  }
+
+  // Create field
+  @Post()
+  addField(@Body() body: { name: string }) {
+    return this.fieldService.addField(body.name);
+  }
+
+  // Delete field
+  @Delete(':name')
+  removeField(@Param('name') name: string) {
+    return this.fieldService.removeField(name);
+  }
+
+  // Update field name
+  @Patch()
+  updateField(
+    @Body() body: { oldName: string; newName: string },
+  ) {
+    return this.fieldService.updateFieldName(
+      body.oldName,
+      body.newName,
+    );
+  }
+}
