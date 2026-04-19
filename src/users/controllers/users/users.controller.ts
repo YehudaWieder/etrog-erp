5d7f1c9d9d4af12f851ed4fa5df4562ba7b9c420
YehudaWieder <v4105677@gmail.com>
2026-04-19 12:37:07 +0300

    Add users controller

--- src/users/controllers/users/users.controller.ts
+++ src/users/controllers/users/users.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+// src/users/controllers/users/users.controller.ts
+
+import { Controller, Get, Post, Patch, Delete, Body, Param, ParseIntPipe, Query } from '@nestjs/common';
+import { UsersService } from '../../services/users/users.service';
+import { Prisma } from '@prisma/client';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: Prisma.UserCreateInput) {
+    return this.usersService.createUser(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Get(':slug')
+  findOne(@Param('slug') slug: string) {
+    return this.usersService.findOne({ slug });
+  }
+
+  @Patch(':id')
+  update(@Param('id', ParseIntPipe) id: number, @Body() updateData: any) {
+    return this.usersService.updateUser(id, updateData);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.usersService.remove(id);
+  }
+}
