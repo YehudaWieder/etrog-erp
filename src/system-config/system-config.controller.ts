cff88fc9674c3240f954cda3c33ea2ba63c1366b
YehudaWieder <v4105677@gmail.com>
2026-04-19 11:10:37 +0300

    Refactor: extract fields into separate model and update services & controllers accordingly

--- src/system-config/system-config.controller.ts
+++ src/system-config/system-config.controller.ts
@@ -1,36 +1,4 @@
-import { Controller, Get, Post, Patch, Delete, Body, Param, ParseIntPipe } from '@nestjs/common';
-import { SystemConfigService } from 'src/system-config/system-config.service';
+import { Controller } from '@nestjs/common';
 
 @Controller('system-config')
-export class SystemConfigController {
-  constructor(private readonly configService: SystemConfigService) {}
-
-  @Get(':seasonId')
-  getConfig(@Param('seasonId', ParseIntPipe) seasonId: number) {
-    return this.configService.getConfig(seasonId);
-  }
-
-  @Patch(':seasonId')
-  updateConfig(
-    @Param('seasonId', ParseIntPipe) seasonId: number,
-    @Body() updateData: any,
-  ) {
-    return this.configService.updateConfig(seasonId, updateData);
-  }
-
-  @Post(':seasonId/fields')
-  addField(
-    @Param('seasonId', ParseIntPipe) seasonId: number,
-    @Body('fieldName') fieldName: string,
-  ) {
-    return this.configService.addField(seasonId, fieldName);
-  }
-
-  @Delete(':seasonId/fields')
-  removeField(
-    @Param('seasonId', ParseIntPipe) seasonId: number,
-    @Body('fieldName') fieldName: string,
-  ) {
-    return this.configService.removeField(seasonId, fieldName);
-  }
-}
+export class SystemConfigController {}
--- src/system-config/system-config.module.ts
+++ src/system-config/system-config.module.ts
@@ -2,9 +2,13 @@
 import { Module } from '@nestjs/common';
 import { SystemConfigService } from './system-config.service';
 import { SystemConfigController } from 'src/system-config/system-config.controller';
+import { ConfigService } from './services/config/config.service';
+import { FieldsService } from './services/fields/fields.service';
+import { FieldsController } from './controllers/fields/fields.controller';
+import { ConfigController } from './controllers/config/config.controller';
 
 @Module({
-  controllers: [SystemConfigController],
-  providers: [SystemConfigService],
+  controllers: [SystemConfigController, FieldsController, ConfigController],
+  providers: [SystemConfigService, ConfigService, FieldsService],
 })
 export class SystemConfigModule {}
