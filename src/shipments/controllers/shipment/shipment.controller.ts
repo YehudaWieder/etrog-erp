720e910e592ef3443cbf8efd033baaec2770e19c
YehudaWieder <v4105677@gmail.com>
2026-04-20 15:00:22 +0300

    Add shipment controller

--- src/shipments/controllers/shipment/shipment.controller.ts
+++ src/shipments/controllers/shipment/shipment.controller.ts
@@ -1,4 +1,43 @@
-import { Controller } from '@nestjs/common';
+// src/shipments/controllers/shipment/shipment.controller.ts
 
-@Controller('shipment-core')
-export class ShipmentController {}
+import { Controller, Get, Post, Patch, Delete, Body, Param, ParseIntPipe, Query } from '@nestjs/common';
+import { ShipmentService } from '../../services/shipment/shipment.service';
+import { Prisma } from '@prisma/client';
+
+@Controller('shipments')
+export class ShipmentController {
+  constructor(private readonly shipmentService: ShipmentService) {}
+
+  @Post()
+  create(@Body() data: Prisma.ShipmentUncheckedCreateInput) {
+    return this.shipmentService.create(data);
+  }
+
+  @Get()
+  findAll(@Query('seasonId', ParseIntPipe) seasonId: number) {
+    return this.shipmentService.findAllBySeason(seasonId);
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.shipmentService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateData: Prisma.ShipmentUncheckedUpdateInput,
+  ) {
+    return this.shipmentService.update(id, updateData);
+  }
+
+  @Patch(':id/recalculate')
+  recalculate(@Param('id', ParseIntPipe) id: number) {
+    return this.shipmentService.updateTotals(id);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.shipmentService.remove(id);
+  }
+}
