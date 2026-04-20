c5a8993d522c3fae1c23d3b9a360f358f09f0569
YehudaWieder <v4105677@gmail.com>
2026-04-20 13:43:54 +0300

    Add harvest controller

--- src/harvest/harvest.controller.ts
+++ src/harvest/harvest.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+// src/harvest/harvest.controller.ts
 
-@Controller('harvest')
-export class HarvestController {}
+import { Controller, Get, Post, Patch, Delete, Body, Param, ParseIntPipe, Query } from '@nestjs/common';
+import { HarvestService } from './harvest.service';
+import { Prisma } from '@prisma/client';
+
+@Controller('harvests')
+export class HarvestController {
+  constructor(private readonly harvestService: HarvestService) {}
+
+  @Post()
+  create(@Body() data: Prisma.FieldHarvestUncheckedCreateInput) {
+    return this.harvestService.create(data);
+  }
+
+  @Get()
+  findAll(@Query('seasonId', ParseIntPipe) seasonId: number) {
+    return this.harvestService.findAllBySeason(seasonId);
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.harvestService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateData: Prisma.FieldHarvestUncheckedUpdateInput,
+  ) {
+    return this.harvestService.update(id, updateData);
+  }
+
+  @Delete(':id')
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.harvestService.remove(id);
+  }
+}
