f6a0818454cd5f53132f0d14b4582307e5457f34
YehudaWieder <v4105677@gmail.com>
2026-04-26 14:46:54 +0300

    Fix users service and controller imports

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import { UsersController } from './controllers/users/users.controller';
+import { UsersService } from './services/users/users.service';
 import { AuthController } from './controllers/auth/auth.controller';
 
 @Module({
