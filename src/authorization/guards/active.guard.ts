0f1bc0d07118414c79519eb8310c8a3d85025133
YehudaWieder <v4105677@gmail.com>
2026-04-26 15:15:34 +0300

    Fix(auth): make ActiveGuard skip checks on routes marked with @Public

--- src/authorization/guards/active.guard.ts
+++ src/authorization/guards/active.guard.ts
@@ -5,10 +5,23 @@ import {
   Injectable,
   UnauthorizedException,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '../../auth/decorators/public.decorator';
 
 @Injectable()
 export class ActiveGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
   canActivate(context: ExecutionContext): boolean {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
+
     const request = context.switchToHttp().getRequest<Express.Request>();
     const user = request.user;
 
