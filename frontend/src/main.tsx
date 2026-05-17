b342d21a43262b216f68da800a5d1af8f0e456e3
YehudaWieder <v4105677@gmail.com>
2026-05-17 15:51:12 +0300

    feat(routing): support /shipments/* subroutes in router

---
+++ frontend/src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { ShipmentsRoute } from './app/routes/ShipmentsRoute';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/shipments/*" element={<ShipmentsRoute />} />
+        <Route path="*" element={<Navigate to="/shipments" replace />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
