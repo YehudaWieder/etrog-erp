da7bd7f1fb49ddb70607ef3c21fbe26e4e634980
YehudaWieder <v4105677@gmail.com>
2026-05-17 20:34:49 +0300

    feat: add /login route to router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { ShipmentsRoute } from './app/routes/ShipmentsRoute';
+import { LoginRoute } from './app/routes/LoginRoute';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<LoginRoute />} />
         <Route path="/shipments/*" element={<ShipmentsRoute />} />
         <Route path="*" element={<Navigate to="/shipments" replace />} />
       </Routes>
