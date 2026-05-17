24f35befb9d6dcc4bdcdcd28bdc620011a7e8457
YehudaWieder <v4105677@gmail.com>
2026-05-17 20:39:41 +0300

    feat: add /register route to router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,12 +4,14 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { ShipmentsRoute } from './app/routes/ShipmentsRoute';
 import { LoginRoute } from './app/routes/LoginRoute';
+import { RegisterRoute } from './app/routes/RegisterRoute';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<LoginRoute />} />
+        <Route path="/register" element={<RegisterRoute />} />
         <Route path="/shipments/*" element={<ShipmentsRoute />} />
         <Route path="*" element={<Navigate to="/shipments" replace />} />
       </Routes>
